cb0fd5406354291167b5ebe883bc3065ccd84176
codex-backupforce <zroaneo22@gmail.com>
2026-01-07 14:59:25 +0800

    Add cached permission provider

---
+++ src/Application/Abstractions/Authorization/IPermissionProvider.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Abstractions.Authorization;
+
+public interface IPermissionProvider
+{
+    Task<bool> HasPermissionAsync(Guid userId, string permissionCode, Guid? nodeId);
+}
